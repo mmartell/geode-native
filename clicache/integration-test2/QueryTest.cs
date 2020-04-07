08d6982e5bbaa9dc034d8b916a132feff76b3276
Mike Martell <mmartell@pivotal.io>
2020-04-07 08:58:23 -0700

    Update QueryTest to use autoserializer

--- clicache/integration-test2/QueryTest.cs
+++ clicache/integration-test2/QueryTest.cs
@@ -21,177 +21,195 @@ using Xunit;
 using System.Diagnostics;
 using System.Threading;
 using Xunit.Abstractions;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Apache.Geode.Client.IntegrationTests
 {
-    public class QueryOrder : IPdxSerializable
+  public class QueryOrder
+  {
+    private const string ORDER_ID_KEY_ = "order_id";
+    private const string NAME_KEY_ = "name";
+    private const string QUANTITY_KEY_ = "quantity";
+    public long OrderId { get; set; }
+    public string Name { get; set; }
+    public short Quantity { get; set; }
+    // A default constructor is required for deserialization
+    public QueryOrder() { }
+    public QueryOrder(int orderId, string name, short quantity)
     {
-        private const string ORDER_ID_KEY_ = "order_id";
-        private const string NAME_KEY_ = "name";
-        private const string QUANTITY_KEY_ = "quantity";
-        public long OrderId { get; set; }
-        public string Name { get; set; }
-        public short Quantity { get; set; }
-        // A default constructor is required for deserialization
-        public QueryOrder() { }
-        public QueryOrder(int orderId, string name, short quantity)
-        {
-            OrderId = orderId;
-            Name = name;
-            Quantity = quantity;
-        }
-        public override string ToString()
-        {
-            return string.Format("Order: [{0}, {1}, {2}]", OrderId, Name, Quantity);
-        }
-        public void ToData(IPdxWriter output)
-        {
-            output.WriteLong(ORDER_ID_KEY_, OrderId);
-            output.MarkIdentityField(ORDER_ID_KEY_);
-            output.WriteString(NAME_KEY_, Name);
-            output.MarkIdentityField(NAME_KEY_);
-            output.WriteInt(QUANTITY_KEY_, Quantity);
-            output.MarkIdentityField(QUANTITY_KEY_);
-        }
-        public void FromData(IPdxReader input)
-        {
-            OrderId = input.ReadLong(ORDER_ID_KEY_);
-            Name = input.ReadString(NAME_KEY_);
-            Quantity = (short)input.ReadInt(QUANTITY_KEY_);
-        }
-        public static IPdxSerializable CreateDeserializable()
-        {
-            return new QueryOrder();
-        }
+      OrderId = orderId;
+      Name = name;
+      Quantity = quantity;
+    }
+    public override string ToString()
+    {
+      return string.Format("Order: [{0}, {1}, {2}]", OrderId, Name, Quantity);
+    }
+    //public void ToData(IPdxWriter output)
+    //{
+    //  output.WriteLong(ORDER_ID_KEY_, OrderId);
+    //  output.MarkIdentityField(ORDER_ID_KEY_);
+    //  output.WriteString(NAME_KEY_, Name);
+    //  output.MarkIdentityField(NAME_KEY_);
+    //  output.WriteInt(QUANTITY_KEY_, Quantity);
+    //  output.MarkIdentityField(QUANTITY_KEY_);
+    //}
+    //public void FromData(IPdxReader input)
+    //{
+    //  OrderId = input.ReadLong(ORDER_ID_KEY_);
+    //  Name = input.ReadString(NAME_KEY_);
+    //  Quantity = (short)input.ReadInt(QUANTITY_KEY_);
+    //}
+    //public static IPdxSerializable CreateDeserializable()
+    //{
+    //  return new QueryOrder();
+    //}
+  }
+
+  [Trait("Category", "Integration")]
+  public class QueryTest : TestBase
+  {
+    public QueryTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+    {
     }
 
-
-    [Trait("Category", "Integration")]
-    public class QueryTest : TestBase
+    [Fact]
+    public void PdxSerializableQueryHaveCorrectValues()
     {
-        public QueryTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+      using (var cluster = new Cluster(output, CreateTestCaseDirectoryName(), 1, 1))
+      {
+        Assert.True(cluster.Start());
+        Assert.Equal(0, cluster.Gfsh.create()
+            .region()
+            .withName("cqTestRegion")
+            .withType("REPLICATE")
+            .execute());
+
+        var properties = new Dictionary<string, string>()
+                    {
+                        { "log-level", "debug" },
+                        { "log-file", "c:/temp/querytest.log" }
+                    };
+
+        var cache = cluster.CreateCache(properties);
+        try
         {
-        }
 
-        [Fact]
-        public void PdxSerializableQueryHaveCorrectValues()
+          //cache.TypeRegistry.RegisterPdxType(QueryOrder.CreateDeserializable);
+          cache.TypeRegistry.PdxSerializer = new ReflectionBasedAutoSerializer();
+
+          var regionFactory = cache.CreateRegionFactory(RegionShortcut.PROXY)
+              .SetPoolName("default");
+
+          var region = regionFactory.Create<string, QueryOrder>("cqTestRegion");
+
+          Debug.WriteLine("Putting and changing Position objects in the region");
+          var order1 = new QueryOrder(1, "product x", 10);
+          var order2 = new QueryOrder(2, "product y", 20);
+          var order3 = new QueryOrder(3, "product z", 30);
+
+          region.Put("order1", order1);
+
+          region.Put("order2", order2);
+
+          region.Put("order3", order3);
+
+          order1.Quantity = 51;
+          region.Put("order1", order1);
+
+          order2.Quantity = UInt16.MaxValue/2 - 3;
+          region.Put("order2", order2);
+
+          order3.Quantity = UInt16.MaxValue/2 - 4;
+          region.Put("order3", order3);
+
+          var res = region.Get("order3");
+
+          var results = region.Query<QueryOrder>("SELECT * FROM /cqTestRegion WHERE quantity > 50");
+          Assert.Equal(results.Size, 2UL);
+          Assert.Equal(results[0].Name, "product y");
+          Assert.Equal(results[1].Name, "product z");
+
+          region.Clear();
+        }
+        finally
         {
-            using (var cluster = new Cluster(output, CreateTestCaseDirectoryName(), 1, 1))
-            {
-                Assert.True(cluster.Start());
-                Assert.Equal(0, cluster.Gfsh.create()
-                    .region()
-                    .withName("cqTestRegion")
-                    .withType("REPLICATE")
-                    .execute());
-
-                var cache = cluster.CreateCache();
-                try
-                {
-
-                    cache.TypeRegistry.RegisterPdxType(QueryOrder.CreateDeserializable);
-
-                    var regionFactory = cache.CreateRegionFactory(RegionShortcut.PROXY)
-                        .SetPoolName("default");
-
-                    var region = regionFactory.Create<string, QueryOrder>("cqTestRegion");
-
-                    Debug.WriteLine("Putting and changing Position objects in the region");
-                    var order1 = new QueryOrder(1, "product x", 23);
-                    var order2 = new QueryOrder(2, "product y", 37);
-                    var order3 = new QueryOrder(3, "product z", 101);
-
-                    region.Put("order1", order1);
-
-                    region.Put("order2", order2);
-
-                    region.Put("order3", order3);
-
-                    order1.Quantity = 20;
-                    region.Put("order1", order1);
-
-                    order2.Quantity = 45;
-                    region.Put("order2", order2);
-
-                    order3.Quantity = 11;
-                    region.Put("order3", order3);
-
-                    var results = region.Query<QueryOrder>("SELECT * FROM /cqTestRegion WHERE quantity > 30");
-                    Assert.Equal(results.Size, 1UL);
-                    Assert.Equal(results[0].Name, "product y");
-
-                    region.Clear();
-                }
-                finally
-                {
-                    cache.Close();
-                }
-            }
+          cache.Close();
         }
-
-        [Fact]
-        public void DataSerializableQueryHaveCorrectValues()
-        {
-            using (var cluster = new Cluster(output, CreateTestCaseDirectoryName(), 1, 1))
-            {
-                Assert.Equal(cluster.Start(), true);
-                Assert.Equal(0, cluster.Gfsh.deploy()
-                    .withJar(Config.JavaobjectJarPath)
-                    .execute());
-                Assert.Equal(0, cluster.Gfsh.create()
-                    .region()
-                    .withName("cqTestRegion")
-                    .withType("REPLICATE")
-                    .execute());
-
-                Assert.Equal(0, cluster.Gfsh.executeFunction()
-                    .withId("InstantiateDataSerializable")
-                    .withMember("DataSerializableQueryHaveCorrectValues_server_0")
-                    .execute());
-
-                var cache = cluster.CreateCache();
-                try {
-                cache.TypeRegistry.RegisterType(Position.CreateDeserializable, 22);
-
-                var regionFactory = cache.CreateRegionFactory(RegionShortcut.PROXY)
-                    .SetPoolName("default");
-
-                var region = regionFactory.Create<string, Position>("cqTestRegion");
-
-                Debug.WriteLine("Putting and changing Position objects in the region");
-                var order1 = new Position("GOOG", 23);
-                var order2 = new Position("IBM", 37);
-                var order3 = new Position("PVTL", 101);
-
-                region.Put("order1", order1);
-                var Value = region["order1"];
-
-                region.Put("order2", order2);
-
-                order1.SharesOutstanding = 55;
-                region.Put("order1", order1);
-
-                order2.SharesOutstanding = 77;
-                region.Put("order2", order2);
-
-                order2.SharesOutstanding = 11;
-                region.Put("order2", order2);
-
-                region.Remove("order1");
-
-                region.Put("order3", order3);
-
-                var results = region.Query<Position>("SELECT * FROM /cqTestRegion WHERE sharesOutstanding > 50");
-                Assert.Equal(results.Size, 1UL);
-                Assert.Equal(results[0].SecId, "PVTL");
-
-                region.Clear();
-                }
-                finally
-                {
-                    cache.Close();
-                }
-            }
-        }
+      }
     }
+
+    [Fact]
+    public void DataSerializableQueryHaveCorrectValues()
+    {
+      using (var cluster = new Cluster(output, CreateTestCaseDirectoryName(), 1, 1))
+      {
+        Assert.Equal(cluster.Start(), true);
+        Assert.Equal(0, cluster.Gfsh.deploy()
+            .withJar(Config.JavaobjectJarPath)
+            .execute());
+        Assert.Equal(0, cluster.Gfsh.create()
+            .region()
+            .withName("cqTestRegion")
+            .withType("REPLICATE")
+            .execute());
+
+        Assert.Equal(0, cluster.Gfsh.executeFunction()
+            .withId("InstantiateDataSerializable")
+            .withMember("DataSerializableQueryHaveCorrectValues_server_0")
+            .execute());
+
+        var properties = new Dictionary<string, string>()
+                    {
+                        { "log-level", "debug" },
+                        { "log-file", "c:/temp/autoserializertest.log" }
+                    };
+
+        var cache = cluster.CreateCache(properties);
+        try
+        {
+          cache.TypeRegistry.RegisterType(Position.CreateDeserializable, 22);
+
+          var regionFactory = cache.CreateRegionFactory(RegionShortcut.PROXY)
+              .SetPoolName("default");
+
+          var region = regionFactory.Create<string, Position>("cqTestRegion");
+
+          Debug.WriteLine("Putting and changing Position objects in the region");
+          var order1 = new Position("GOOG", 23);
+          var order2 = new Position("IBM", 37);
+          var order3 = new Position("PVTL", 101);
+
+          region.Put("order1", order1);
+          var Value = region["order1"];
+
+          region.Put("order2", order2);
+
+          order1.SharesOutstanding = 55;
+          region.Put("order1", order1);
+
+          order2.SharesOutstanding = 77;
+          region.Put("order2", order2);
+
+          order2.SharesOutstanding = 11;
+          region.Put("order2", order2);
+
+          region.Remove("order1");
+
+          region.Put("order3", order3);
+
+          var results = region.Query<Position>("SELECT * FROM /cqTestRegion WHERE sharesOutstanding > 50");
+          Assert.Equal(results.Size, 1UL);
+          Assert.Equal(results[0].SecId, "PVTL");
+
+          region.Clear();
+        }
+        finally
+        {
+          cache.Close();
+        }
+      }
+    }
+  }
 }
